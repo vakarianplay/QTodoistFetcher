3c8847dc15f5d1cb5664ee6df0657895a4a0d771
Kostya Vakarian <vakarianplay@gmail.com>
2025-09-24 17:37:52 +0300

    Changes in tg API requests

--- tgsender.cpp
+++ tgsender.cpp
@@ -10,35 +10,27 @@ TgSender::TgSender(QString message, QObject *parent)
     sendMessageToTelegramBot(botToken, message, userId);
 }
 
-void TgSender::sendMessageToTelegramBot(const QString &botToken, const QString &message, const QString &userId) {
+void TgSender::sendMessageToTelegramBot(const QString &botToken, const QString &message, const QString &userId)
+{
     QNetworkAccessManager *manager = new QNetworkAccessManager();
-
-    // URL для отправки сообщения через Telegram Bot API
     QString apiUrl = QString("https://api.telegram.org/bot%1/sendMessage").arg(botToken);
-
-    // Подготовка запроса
     QNetworkRequest request((QUrl(apiUrl)));
     request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
 
-    // Тело запроса в формате JSON
     QJsonObject jsonBody;
-    jsonBody["chat_id"] = userId;               // ID пользователя
-    jsonBody["text"] = message;                // Сообщение
+    jsonBody["chat_id"] = userId;
+    jsonBody["text"] = message;
 
     QJsonDocument jsonDoc(jsonBody);
     QByteArray requestData = jsonDoc.toJson();
-
-    // Выполнение POST-запроса
     QNetworkReply *reply = manager->post(request, requestData);
-
-    // Обработка ответа от Telegram API
     QObject::connect(reply, &QNetworkReply::finished, [reply]() {
         if (reply->error() == QNetworkReply::NoError) {
-            qDebug() << "Сообщение успешно отправлено!";
+            qDebug() << "Messages send!";
             qDebug() << reply->readAll();
         } else {
-            qDebug() << "Ошибка отправки сообщения:" << reply->errorString();
+            qDebug() << "Error:" << reply->errorString();
         }
-        reply->deleteLater(); // Освобождение памяти
+        reply->deleteLater();
     });
 }
